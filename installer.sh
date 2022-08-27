8853014a45d56b0c508d1fa08373491160a63341
Malte Kiefer <malte.kiefer@intellytec.de>
2022-08-27 07:13:51 +0200

    remove sudo and add root check

--- installer.sh
+++ installer.sh
@@ -21,22 +21,27 @@ OS=$(lsb_release -d | awk -F"\t" '{print $2}' | awk -F" " '{print $1}')
 
 ##################################################################################################
 
+if [ "$EUID" -ne 0 ]
+	then echo "This script must be run as root"
+	exit
+fi
+
 if [[ $OS == "Ubuntu" || $OS == "Debian" ]]; then
-	sudo apt-get update
-	sudo apt-get -y install software-properties-common python3-pip python3-distro ca-certificates wget
+	apt-get update
+	apt-get -y install software-properties-common python3-pip python3-distro ca-certificates wget
 
 	pip3 install pyOpenSSL --upgrade
 
 	mkdir -p /opt/fastcp
 	cd /opt/fastcp
 
-	sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
-	sudo debconf-set-selections <<< "postfix postfix/mailname string $HOSTNAME"
-	sudo wget -nv -O /usr/bin/fastcp-updater https://raw.githubusercontent.com/FastCP/Installer/master/fastcp-updater.py
-	sudo chmod +x /usr/bin/fastcp-updater
-	sudo wget -nv -O fastcp-installer https://raw.githubusercontent.com/FastCP/Installer/master/fastcp-installer.py
-	sudo chmod +x fastcp-installer
-	sudo ./fastcp-installer
+	debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+	debconf-set-selections <<< "postfix postfix/mailname string $HOSTNAME"
+	wget -nv -O /usr/bin/fastcp-updater https://raw.githubusercontent.com/FastCP/Installer/master/fastcp-updater.py
+	chmod +x /usr/bin/fastcp-updater
+	wget -nv -O fastcp-installer https://raw.githubusercontent.com/FastCP/Installer/master/fastcp-installer.py
+	chmod +x fastcp-installer
+	./fastcp-installer
 else
 	echo "FastCP is currently only usable with Debian or Ubuntu. The installer will now exit"
 	exit
